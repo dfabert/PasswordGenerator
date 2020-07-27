3dcdf9e213c382c97f04e1f6acb1d0eed596c2eb
dfabert <glen.fabert@gmail.com>
2020-07-26 22:25:00 -0400

    Updated the confirming message portion with appropriate puncuation.

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
 function userOptions(){
     var passLength = prompt("How many characters do you want in your password?  You can pick between 8 and 128");
         if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-        alert("You did not pick a number between 8 and 128.  Please refresh and start over");
-        userOptions();
-    }
+            alert("You did not pick a number between 8 and 128.  You will be returned to the beginning.");
+            userOptions();
+        }
     var passCase = confirm("Would you like uppercase included? (Press 'ok' for yes and 'cancel' for no)");
     var passNum = confirm("Would you like numbers included? (Press 'ok' for yes and 'cancel' for no)");
     var passSpecial = confirm("Would you like special characters included? (Press 'ok' for yes and 'cancel' for no)");
-    var correct = confirm('You selected ' + passLength + " characters. Press 'ok' for yes and 'cancel' for to return to the beginning of the options" );
+
+    var confirmMessage = ['lowercase letters'];
+        if(passCase || passNum || passSpecial){
+            if(passCase){
+                confirmMessage.push('uppercase letters');
+                console.log(confirmMessage);
+            }
+            if(passNum){
+                confirmMessage.push('numbers');
+                console.log(confirmMessage);
+            }
+            if(passSpecial){
+                confirmMessage.push('special characters');
+                console.log(confirmMessage);
+            }
+            if(confirmMessage.length == 2){
+                var lastPart = confirmMessage[confirmMessage.length-1]   //These three lines are to get the "and" in the right spot
+                confirmMessage.push(lastPart)
+                confirmMessage[confirmMessage.length-2]='and '
+                confirmMessage = confirmMessage.join(' ');   
+            }
+            if(confirmMessage.length == 3){
+                var lastPart = confirmMessage[confirmMessage.length-1]   //These three lines are to get the "and" in the right spot
+                confirmMessage.push(lastPart)
+                confirmMessage[confirmMessage.length-2]='and '
+                confirmMessage.splice(1,0, ", ");
+                confirmMessage.splice(3,0, ", ");
+                confirmMessage = confirmMessage.join('');
+            }
+
+            if(confirmMessage.length == 4){
+                var lastPart = confirmMessage[confirmMessage.length-1]   //These three lines are to get the "and" in the right spot
+                confirmMessage.push(lastPart)
+                confirmMessage[confirmMessage.length-2]='and '
+                confirmMessage.splice(1,0, ", ");
+                confirmMessage.splice(3,0, ", ");
+                confirmMessage.splice(5,0, ", ");
+                confirmMessage = confirmMessage.join('');
+            }
+        }else confirmMessage = 'only lowercase letters'
+
+    var correct = confirm("You selected " + passLength + " characters to be made up of " + confirmMessage + ".  Press 'ok' to generate password or 'cancel' for to return to the beginning of the options");
     
     if(correct){
         //If statements that add to characters variable 
@@ -20,18 +61,13 @@ function userOptions(){
         function generateCharset() {
             if (passCase)
                 charSet = charSet.concat(UpperCaseChars);
-                console.log(charSet);
             if (passNum)
                 charSet = charSet.concat(numberChars);
-                console.log(charSet);
             if (passSpecial)
                 charSet = charSet.concat(specialChars);
-                console.log(charSet);
         }
 
-
         function generatePassword() {
-            console.log(charSet);
             charSet = charSet.split('');
 
             //create first letter of password
@@ -39,25 +75,17 @@ function userOptions(){
             var password = randomChar;
 
             //create other letters of password
-            for (var i = 0; i < passLength; i++){
+            for (var i = 1; i < passLength; i++){
             var randomChar = charSet[Math.floor(Math.random()*charSet.length)];
             console.log(randomChar);
             var password = password.concat(randomChar);
             }
-
-            console.log(password)
             alert('Your password is:  ' + password);
         }
     }
     else
-        userOptions();;
-generateCharset();
-generatePassword();
+        userOptions();
+    generateCharset();
+    generatePassword();
 }
-
-
-
-
 userOptions();
-
-//Place password on page
